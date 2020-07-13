aac62bda6c18964909a50cce15f5373a430efd25
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2020-07-13 18:18:05 +0300

    First stage

--- Board.js
+++ Board.js
@@ -1,18 +1,68 @@
-function Board() {
-    this.board = convertArrayToBoard(
-        Board.START_BOARD
-        .sort(() => Math.random() - 0.5)
-    );
+class Board {
+    START_BOARD = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 'empty'];
+    constructor() {
+        this.board = this.convertArrayToBoard(this.START_BOARD
+            .sort(() => Math.random() - 0.5)
+        );
+    }
+    convertArrayToBoard = function (boardArray) {
+        return boardArray.reduce((board, cell, idx) => {
+            board[idx] = cell;
+            return board;
+        }, {});
+    }
+    render = function () {
+        console.log('----- board start------');
+        for (let row = 0; row < 4; row++) {
+            let rowStr = '';
+            for (let col = 0; col < 4; col++) {
+                const el = this.board[col + row * 4];
+                if (el === 'empty') {
+                    rowStr += 'ee ';
+                } else {
+                    rowStr += el.toString().padStart(2, '0') + ' ';
+                }
+            }
+            console.log(rowStr);
+        }
+
+        return '----- board end------';
+    }
+    canMove = function (number) {
+        if (number === 'empty') return null;
+        const currentIndex = this.getIndex(number),
+            sublingsItems = this.getSiblingsIndex(currentIndex),
+            possibleMove = ['LEFT', 'RIGHT', 'TOP', 'BOTTOM']
+            .find(direction => sublingsItems[direction] != null && this.board[sublingsItems[direction]] === 'empty');
+        return possibleMove;
+    }
+    getIndex = function (number) {
+        for (let index = 0; index < 16; index++) {
+            if (this.board[index] === number) return index;
+        }
+    }
+    getSiblingsIndex = function (currentIndex) {
+        const leftItemIndex = currentIndex % 4 === 0 ? null : currentIndex - 1,
+            rightItemIndex = currentIndex % 4 === 3 ? null : currentIndex + 1,
+            topItemIndex = currentIndex < 4 ? null : currentIndex - 4,
+            bottomItemIndex = currentIndex > 11 ? null : currentIndex + 4;
+        return {
+            LEFT: leftItemIndex,
+            RIGHT: rightItemIndex,
+            TOP: topItemIndex,
+            BOTTOM: bottomItemIndex
+        }
+    }
 }
 
-Board.START_BOARD = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 'empty'];
+const game = new Board();
 
-function convertArrayToBoard(boardArray) {
-    return boardArray.reduce((board, cell, idx) => {
-        board[idx] = cell;
-        return board;
-    }, {});
+console.log(game);
+console.log(game.render());
+
+
+for (let value in game.board) {
+    if (game.canMove(game.board[value]) != undefined) {
+        console.log(game.board[value], game.canMove(game.board[value]));
+    }
 }
-
-const board = new Board();
-console.log(board);
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
-# 🎲 15 Puzzle
-A simple 15 Puzzle game in plain JavaScript
+# 🎲 Fifteen Puzzle
+The **"15-puzzle"** game (also called Gem Puzzle, Boss Puzzle, Game of Fifteen, Mystic Square and many others) in plain JavaScript.
+
+![Demo](./img/fifteen-puzzle-demo.gif)
 
 ### [About game](https://en.wikipedia.org/wiki/15_puzzle) | [View the Demo](https://icherya.github.io/Fifteen-Puzzle/)
 
